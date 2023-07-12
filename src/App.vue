99fa4713ea140d9dbcd74d5d8b6f10ca9991a871
michael-kestler <michael.s.kestler@gmail.com>
2023-07-12 16:38:52 -0400

    added template and ul along with object to app.vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        <li></li>
+        <li></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data(){
+        return {
+        storedResources: [
+            { id: 'official-guide', 
+            title: 'Official Guide',
+            description: 'The official Vue.js documentation.',
+            link: 'https://vuejs.org'
+
+            },
+            {
+                id: 'google',
+                title: 'Google',
+                description: 'Learn to google...',
+                link: 'https://google.com'
+            }
+        ]
+    }
+}
+}
+</script>
