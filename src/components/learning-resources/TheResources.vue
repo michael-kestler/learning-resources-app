9a6670dee0f7e67f88bb7db9ad1401cec78b2254
michael-kestler <michael.s.kestler@gmail.com>
2023-07-14 17:05:37 -0400

    registered stored resources in the resource instead of app.vue

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -3,17 +3,23 @@
     <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
     <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
   </base-card>
+  <component :is="selectedTab"></component>
 </template>
-
 <script>
+import StoredResources from './components/learning-resources/StoredResources.vue'
+
 export default {
+    components: {
+         StoredResources
+    },   
+
   data() {
     return {
       selectedTab: 'stored-resources',
     };
   },
   methods: {
-    setSelectedTab: 'stored-resources',
+    setSelectedTab: 'add-resource',
   },
 };
 </script>
