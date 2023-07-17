0a04f7f9d53fba2136a906533630a78d156a798d
michael-kestler <michael.s.kestler@gmail.com>
2023-07-17 16:20:24 -0400

    Added form for the user to input title, description, and url

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,3 +1,21 @@
 <template>
-    <h2>Add Resource</h2>
+   <base-card>
+       <form>
+        <div class="form-control">
+            <label for="title" >Title</label>
+            <input id="title" name="title" type="text">
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea id="description" name="description" rows="3"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="link" >Link</label>
+            <input id="link" name="link" type="url">
+        </div>
+        <div>
+            <base-button type="submit">Add Resource</base-button>
+        </div>
+       </form>
+   </base-card>
 </template>
