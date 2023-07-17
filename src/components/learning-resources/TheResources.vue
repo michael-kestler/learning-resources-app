eed3e92ad6daf9da348c78a6d511bb832f338f4a
michael-kestler <michael.s.kestler@gmail.com>
2023-07-17 16:08:21 -0400

    bound computed methods to the tabs to highight the one that is clicked on.

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,7 +1,7 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')" :mode="selectedTab === 'stored-resources' ? null : 'flat'">Stored Resources</base-button>
-    <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+    <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
   </base-card>
      <component :is="selectedTab"></component>
  
@@ -39,10 +39,20 @@ export default {
   provide() {
     return {
        resources: this.storedResources
-
-    }
-   
+    } 
   },
+
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode () {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    }
+
+
+  },
+    
   methods: {
     setSelectedTab(tab){
       this.selectedTab = tab;
