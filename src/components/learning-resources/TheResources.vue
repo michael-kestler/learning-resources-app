213fdbc73054e6b9dad5243a7add1fce387d14c8
michael-kestler <michael.s.kestler@gmail.com>
2023-07-20 08:33:17 -0400

    Added function to delete resources using the delete button

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -49,6 +49,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource
     };
   },
 
@@ -76,6 +77,11 @@ export default {
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
     },
+      removeResource(resId) {
+        const resIndex = this.storedResources.findIndex(res => res.id === resId)
+        this.storedResources.splice(resIndex, 1);
+
+      }
   },
 };
 </script>
